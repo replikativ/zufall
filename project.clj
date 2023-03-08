6e865b3c9b8d1e8d6c6ecc58b60101155f52a00d
JMassa <j.massa@lambdaforge.io>
2023-03-08 15:03:10 +0100

    port to deps.edn and common ci

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject io.replikativ/zufall "0.1.0"
-  :description "Random name generators"
-  :url "https://github.com/replikativ/zufall"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns zufall.core})
